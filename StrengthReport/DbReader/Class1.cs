23622e0410a1c31e877f4c4e20e64d2283c45b8e
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-02-10 15:48:19 +0300

    optimase dbReader
    ridan

--- StrengthReport/DbReader/Class1.cs
+++ StrengthReport/DbReader/Class1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data.Common;
+﻿using System.Data.Common;
 
 namespace DbReader
 {
@@ -49,87 +48,6 @@ namespace DbReader
             set { m_page = value; }
         }
 
-        public int[,] GetIntegerTable()
-        {
-            int Columns = m_Fields.Length;
-            System.Collections.ArrayList[] table = new System.Collections.ArrayList[Columns];
-            int i;
-            string value;
-
-            for (i = 0; i < Columns; i++)
-            {
-                table[i] = new System.Collections.ArrayList();
-            }
-
-            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + m_dbPath + @" ; Extended Properties=""Excel 8.0;HDR=YES;""";
-            DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.OleDb");
-            DbConnection connection = factory.CreateConnection();
-
-            connection.ConnectionString = connectionString;
-
-            DbCommand command = connection.CreateCommand();
-            string commandText = "SELECT " + m_Fields[0];
-
-            for (i = 1; i < Columns; i++)
-            {
-                commandText += ", " + m_Fields[i];
-            }
-
-            commandText += " FROM [" + m_page + "$]";
-            command.CommandText = commandText;
-
-            connection.Open();
-
-            DbDataReader dr = command.ExecuteReader();
-
-            while (dr.Read())
-            {
-                double A;
-
-                for (i = 0; i < Columns; i++)
-                {
-                    value = dr[m_Fields[i]].ToString();
-
-                    if (value.Length == 0)
-                    {
-                        A = 0;
-                    }
-                    else
-                    {
-                        if (!double.TryParse(value, out A))
-                        {
-                            A = 0;
-                        }
-                    }
-
-                    int nA = Convert.ToInt32(A);
-
-                    table[i].Add(nA);
-                }
-            }
-
-            connection.Close();
-
-            int Rows = table[0].Count;
-            int[,] IntegerTable = new int[Rows, Columns];
-
-            for (i = 0; i < Rows; i++)
-            {
-                for (int j = 0; j < Columns; j++)
-                {
-                    IntegerTable[i, j] = (int)table[j][i];
-                }
-            }
-
-            // release items from table
-            for (i = 0; i < Columns; i++)
-            {
-                table[i].Clear();
-            }
-
-            return IntegerTable;
-        }
-
         public double[,] GetDoubleTable()
         {
             int Columns = m_Fields.Length;
@@ -142,11 +60,10 @@ namespace DbReader
                 table[i] = new System.Collections.ArrayList();
             }
 
-            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + m_dbPath + @" ; Extended Properties=""Excel 8.0;HDR=YES;""";
             DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.OleDb");
             DbConnection connection = factory.CreateConnection();
 
-            connection.ConnectionString = connectionString;
+            connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + m_dbPath + @" ; Extended Properties=""Excel 8.0;HDR=YES;"""; ;
 
             DbCommand command = connection.CreateCommand();
 
@@ -208,72 +125,6 @@ namespace DbReader
             }
 
             return DoubleTable;
-        }
-
-        public string[,] GetStringTable()
-        {
-            int Columns = m_Fields.Length;
-            System.Collections.ArrayList[] table = new System.Collections.ArrayList[Columns];
-            int i;
-
-            for (i = 0; i < Columns; i++)
-            {
-                table[i] = new System.Collections.ArrayList();
-            }
-
-            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + m_dbPath + @" ; Extended Properties=""Excel 8.0;HDR=YES;""";
-            DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.OleDb");
-            DbConnection connection = factory.CreateConnection();
-
-            connection.ConnectionString = connectionString;
-
-            DbCommand command = connection.CreateCommand();
-
-            string commandText = "SELECT " + m_Fields[0];
-
-            for (i = 1; i < Columns; i++)
-            {
-                commandText += ", " + m_Fields[i];
-            }
-
-            commandText += " FROM [" + m_page + "$]";
-            command.CommandText = commandText;
-
-            connection.Open();
-
-            DbDataReader dr = command.ExecuteReader();
-
-            while (dr.Read())
-            {
-                string A;
-
-                for (i = 0; i < Columns; i++)
-                {
-                    A = dr[m_Fields[i]].ToString();
-                    table[i].Add(A);
-                }
-            }
-
-            connection.Close();
-
-            int Rows = table[0].Count;
-            string[,] StringTable = new string[Rows, Columns];
-
-            for (i = 0; i < Rows; i++)
-            {
-                for (int j = 0; j < Columns; j++)
-                {
-                    StringTable[i, j] = (string)table[j][i];
-                }
-            }
-
-            // release items from table
-            for (i = 0; i < Columns; i++)
-            {
-                table[i].Clear();
-            }
-
-            return StringTable;
         }
 
         #endregion
