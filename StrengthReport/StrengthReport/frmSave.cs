a3e6449e1abeeb8e3d8034fc7a2bdcfe7fe6d212
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-02-20 10:46:33 +0300

    fix bug during save report
    ridan

--- StrengthReport/StrengthReport/frmSave.cs
+++ StrengthReport/StrengthReport/frmSave.cs
@@ -57,9 +57,10 @@ namespace StrengthReport
 
                     if (bEqual)
                         break;
-
-                    result = true;
                 }
+
+                result = true;
+
             } while (false);
 
             return result;
